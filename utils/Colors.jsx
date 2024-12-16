4dbf6d928b890583aa11ac2a3e0aa1dd4d9c4b24
raflimuhammadd <raflimuhh09@gmail.com>
2024-12-16 12:18:57 +0700

    add-new-category

--- utils/Colors.jsx
+++ utils/Colors.jsx
@@ -2,5 +2,11 @@ export default {
   BLACK: '#000000',
   WHITE: '#FFFFFF',
   PRIMARY: '#8B42FC',
-  GRAY: '#F0F0F0',
+  GRAY: '#D4D4D4',
+  BLUE: '#4F75FE',
+  GREEN: '#13C38B',
+  PURPLE: '#9F3CFE',
+  RED: '#FF555D',
+  ORANGE: '#FF7D4F',
+  COLOR_LIST: ['#4F75FE', '#13C38B', '#9F3CFE', '#FF555D', '#FF7D4F']
 }
