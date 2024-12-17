fdf7e6e09c6f99792dc3db11dcc01cfe194541ea
raflimuhammadd <raflimuhh09@gmail.com>
2024-12-17 17:48:18 +0700

    add-category-details

--- app/category-details.jsx
+++ app/category-details.jsx
@@ -1,15 +1,39 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useEffect } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { supabase } from '../utils/SupabaseConfig'
+import { useState } from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import CourseInfo from '../components/CourseDetails/CourseInfo'
+import CourseItemList from '../components/CourseItemList'
 
 export default function CategoryDetails() {
   const { categoryId } = useLocalSearchParams();
+  const [categoryData, setCategoryData] = useState([]);
+  const router = useRouter();
   useEffect(() => {
     console.log("Category ID: ", categoryId)
-  }, [categoryId])
+    categoryId && getCategoryDetails();
+  }, [categoryId]);
+
+  const getCategoryDetails = async () => {
+    const { data, error } = await supabase.from('Category')
+      .select('*,CategoryItems(*)')
+      .eq('id', categoryId)
+    setCategoryData(data[0]);
+
+    // console.log("Cat:", data)
+  }
   return (
-    <View>
-      <Text>category-detail</Text>
+    <View style={{
+      padding: 20,
+      marginTop: 20,
+    }}>
+      <TouchableOpacity onPress={() => router.back()}>
+        <Ionicons name="arrow-back-circle" size={45} color="black" />
+      </TouchableOpacity>
+      <CourseInfo categoryData={categoryData} />
+      <CourseItemList categoryData={categoryData} />
     </View>
   )
 }
